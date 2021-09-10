ed09d0a786d5603755aa8f5a2eb44e6296f6cdce
Yang Zhao <yang.zhao@sap.com>
2021-09-10 09:33:53 -0700

    reproduce useState bug

--- main.js
+++ main.js
@@ -4,20 +4,37 @@ const html = htm.bind(h)
 
 // import { html, render, useState, useEffect } from 'https://unpkg.com/htm@3.1.0/preact/standalone.module.js'
 
+const WaitingPage = function (props) {
+    const [title, setTitle] = useState('Waiting')
+
+    return html`<div>${title}</div>`
+}
+
 const FinalPage = function (props) {
     const [canBackHome, setCanBackHome] = useState(false)
 
     useEffect(() => {
-        setTimeout(() => setCanBackHome(true), 2000)
+        setTimeout(() => setCanBackHome(true), 5000)
     }, [])
 
     const backHomeButton = canBackHome ? html`<div>
-        <button type="button">Click Me!</button>
-    </div>` : ''
+        <button>Click Me!</button>
+    </div>` : null
 
     return html`<div>
         <h1>Hello</h1>
         ${backHomeButton}
     </div>`
 }
-render(html`<${FinalPage} />`, document.body)
+
+const App = function (props) {
+    const [isFinal, setIsFinal] = useState(false)
+    const waitingBlock = !isFinal ? html`<${WaitingPage} />` : null
+    const finalBlock = isFinal ? html`<${FinalPage} />` : null
+    return html`<div>
+        <button onClick=${() => setIsFinal(true)}>Finish</button>
+        ${waitingBlock}
+        ${finalBlock}
+    </div>`
+}
+render(html`<${App} />`, document.body)
